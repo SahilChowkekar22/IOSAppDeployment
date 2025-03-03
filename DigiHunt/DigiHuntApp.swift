0b1d9fe37e0a8206d6fc6982726fc12961ce7d4d
SahilChowkekar22 <Sahil.Chowkekar@techconsulting.tech>
2025-03-03 14:06:48 -0500

    Initial Commit

---
+++ DigiHunt/DigiHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigiHuntApp.swift
+//  DigiHunt
+//
+//  Created by Sahil ChowKekar on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DigiHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
