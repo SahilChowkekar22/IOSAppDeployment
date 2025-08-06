626c43d66328febb6c5e882bdf31a6173145086d
SahilChowkekar22 <Sahil.Chowkekar@techconsulting.tech>
2025-08-06 18:08:10 -0400

    Prepare for first push with rebase

---
+++ DigiHunt/APIService/APIConstants.swift
@@ -0,0 +1,14 @@
+//
+//  APIConstants.swift
+//  DigiHunt
+//
+//  Created by Sahil ChowKekar on 3/3/25.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let DigimonEndPoint = "https://digimon-api.vercel.app/api/digimon"
+}
+
+
--- DigiHunt/DigiHuntApp.swift
+++ DigiHunt/DigiHuntApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct DigiHuntApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            DigimonListView()
         }
     }
 }
